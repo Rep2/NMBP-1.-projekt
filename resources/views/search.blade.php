6b82456e7461a38aa17b737195393cb1861e442f
Ivan Rep <ivan.rep2@gmail.com>
2015-10-20 11:57:10 +0200

    Configuring select

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -104,7 +104,7 @@
                     $newText = "";
 
                     $.each(data["message"], function(index, val) {
-                        $newText += val["title"] + "[" + val["rank"] + "]";
+                        $newText += val["title"] + "[" + val["rank"] + "]\n";
                     });
 
                     document.getElementById("text1").innerHTML = $newText;
