6cce3b0037cc7ab638051ea3708b363a17b8a674
Ivan Rep <ivan.rep2@gmail.com>
2015-10-20 12:16:45 +0200

    Configuring select

--- app/Http/Controllers/Search.php
+++ app/Http/Controllers/Search.php
@@ -96,8 +96,9 @@ class Search extends Controller
 
         $queryString = "'" .implode($separator, $parsedTokenArray). "'";
 
-        $selectQuery = "SELECT id,
-         ts_headline(text, to_tsquery(" .$queryString. ")) title, ts_rank(to_tsvector(text), to_tsquery(" .$queryString. ")) rank FROM texts ";
+        $selectQuery = "SELECT   ts_headline(text, to_tsquery(" .$queryString. ")) title,
+                ts_rank(to_tsvector(text), to_tsquery(" .$queryString. ")) rank
+        FROM texts ";
 
         $separator = $request->input('andor') == 0 ? " AND " : " OR ";
 
