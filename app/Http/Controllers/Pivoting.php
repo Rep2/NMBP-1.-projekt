5b5d514b151606a462500f3db44e5ab165205117
Ivan Rep <ivan.rep2@gmail.com>
2015-10-28 22:29:37 +0100

    SA

--- app/Http/Controllers/Pivoting.php
+++ app/Http/Controllers/Pivoting.php
@@ -42,10 +42,10 @@ class Pivoting extends Controller
         $datDo = \DateTime::createFromFormat('Y-m-d', $request->input("datum_do"));
         $type = $request->input("type");
 
-        $queryString = "SELECT * FROM crosstab (\"SELECT query, CAST(date AS DATE) as newDate, count(*)
+        $queryString = "SELECT * FROM crosstab ('SELECT query, CAST(date AS DATE) as newDate, count(*)
           FROM log
           GROUP BY query, newDate
-          ORDER BY query, newDate\")
+          ORDER BY query, newDate')
           AS pivotTable (query varchar";
 // WHERE date::DATE >= to_date('" .$datOd->format('d-m-Y'). "', 'd-m-Y') AND date::Date <= to_date('" .$datDo->format('d-m-Y'). "', 'd-m-Y')
 
