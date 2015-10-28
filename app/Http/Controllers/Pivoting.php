11b78fe4d91ac4fbc5be473ed422e148160b2222
Ivan Rep <ivan.rep2@gmail.com>
2015-10-28 19:12:19 +0100

    SA

--- app/Http/Controllers/Pivoting.php
+++ app/Http/Controllers/Pivoting.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Validator;
+use Illuminate\Support\Facades\DB;
 
 class Pivoting extends Controller
 {
