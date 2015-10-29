995f2f1aebc4f957e759450efce0ffa55260f9e4
Ivan Rep <ivan.rep2@gmail.com>
2015-10-29 02:30:04 +0100

    SA

--- app/Http/Controllers/Pivoting.php
+++ app/Http/Controllers/Pivoting.php
@@ -69,7 +69,7 @@ class Pivoting extends Controller
             $dateQuery .= " H";
         }
         foreach ($result as $value)
-            $queryString .= ", \"" . get_object_vars($value) . "\" bigint";
+            $queryString .= ", \"" . get_object_vars($value)[0] . "\" bigint";
 
         $queryString .= " ) ORDER BY query";
 
