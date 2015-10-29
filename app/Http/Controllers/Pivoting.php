75c450d5ee35f9fea5b6e99e98f4ab8a18d0f2f4
Ivan Rep <ivan.rep2@gmail.com>
2015-10-29 02:23:27 +0100

    SA

--- app/Http/Controllers/Pivoting.php
+++ app/Http/Controllers/Pivoting.php
@@ -69,7 +69,7 @@ class Pivoting extends Controller
             $dateQuery .= " H";
         }
         foreach ($result as $value)
-            $queryString .= ", \"" . $value["newData"] . "\" bigint";
+            $queryString .= ", \"" . $value->only("newData") . "\" bigint";
 
         $queryString .= " ) ORDER BY query";
 
